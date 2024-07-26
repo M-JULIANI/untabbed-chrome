23a2acdc9c6b09fc9d3560080f32496f4652be5e
Marco Juliani <marco@outerlabs.io>
2024-07-25 21:39:45 -0400

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Toaster } from './components/ui/toaster';
 
 const rootElement = document.getElementById('root');
 
@@ -12,5 +13,6 @@ if (!rootElement) {
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
+    <Toaster />
   </React.StrictMode>
 )
